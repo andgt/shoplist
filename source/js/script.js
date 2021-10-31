cc7a85ce86cb9ddf0529cd616f2f772436c447cc
anatoliy <anatolijdegtyar@yandex.ru>
2021-10-31 22:06:35 +0300

    Стилизует header

---
+++ source/js/script.js
@@ -0,0 +1,68 @@
+'use strict'
+
+// Menu
+
+let mainMenu = function() {
+  let mainNav = document.querySelector(".main-nav");
+  let buttonNav = document.querySelector(".main-nav__button-toggle");
+
+  buttonNav.onclick = function(evt) {
+    if (mainNav.classList.contains("main-nav--closed")) {
+      mainNav.classList.remove("main-nav--closed");
+      mainNav.classList.add("main-nav--opened");
+    } else {
+      mainNav.classList.remove("main-nav--opened");
+      mainNav.classList.add("main-nav--closed");
+    }
+  }
+};
+
+mainMenu();
+
+//Submenu
+
+let submenu = function() {
+  let buttonsSubmenu = document.querySelectorAll(".main-nav__button-submenu");
+
+  buttonsSubmenu.forEach(buttonSub => {
+    buttonSub.onclick = function(evt) {
+      let parentEl = buttonSub.closest(".main-nav__control");
+      let submenu = parentEl.nextElementSibling;
+      if (submenu.classList.contains("main-nav__submenu--closed")) {
+        submenu.classList.remove("main-nav__submenu--closed");
+        submenu.classList.add("main-nav__submenu--opened");
+        buttonSub.classList.add("main-nav__button-submenu--rotate");
+      } else {
+        submenu.classList.remove("main-nav__submenu--opened");
+        submenu.classList.add("main-nav__submenu--closed");
+        buttonSub.classList.remove("main-nav__button-submenu--rotate");
+      }
+    };
+  })
+};
+
+submenu();
+
+// Submenu list
+
+let submenuInner = function() {
+  let submenuListsToggle = document.querySelectorAll(".main-nav__submenu-title");
+
+  submenuListsToggle.forEach(element => {
+    element.onclick = function(evt) {
+      let innerList = element.nextElementSibling;
+      let submenuArrow = element.children[1];
+      if (innerList.classList.contains("main-nav__submenu-list--closed")) {
+        innerList.classList.remove("main-nav__submenu-list--closed");
+        innerList.classList.add("main-nav__submenu-list--opened");
+        submenuArrow.classList.add("main-nav__button-submenu--rotate");
+      } else {
+        innerList.classList.remove("main-nav__submenu-list--opened");
+        innerList.classList.add("main-nav__submenu-list--closed");
+        submenuArrow.classList.remove("main-nav__button-submenu--rotate");
+      }
+    }
+  });
+};
+
+submenuInner();
